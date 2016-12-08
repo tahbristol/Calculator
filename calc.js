6789d7fa2ef6bd1d070e4cbe65fc8831a53d7ea0
Bristol <bristolta@D02017037.niaid.nih.gov>
2016-12-08 12:07:55 -0500

    Changed buttons to p elements. Made a new css file to work out without sass. Added functionality to number buttons

--- calc.js
+++ calc.js
@@ -1,18 +1,81 @@
 $(function() {
 
     //starting with a clear screen and binding the clear button.
-    $('input').val('');
+    $('.screen').text('');
 
     $('#clear').on('click', function() {
-        $('input').val('');
+        $('.screen').text('');
     });
 
 
     $('#1').on('click', function() {
-        $('input').val(1);
+     var num = 1;
+        $('.screen').append(document.createTextNode(num));
 
-    })
+    });
+	
+	$('#2').on('click', function() {
+     var num = 2;
+        $('.screen').append(document.createTextNode(num));
 
+
+    });
+    $('#3').on('click', function() {
+     var num = 3;
+        $('.screen').append(document.createTextNode(num));
+
+
+    });
+
+	$('#4').on('click', function() {
+     var num = 4;
+        $('.screen').append(document.createTextNode(num));
+
+
+    });
+	
+	$('#5').on('click', function() {
+     var num = 5;
+        $('.screen').append(document.createTextNode(num));
+
+
+    });
+
+	$('#6').on('click', function() {
+     var num = 6;
+        $('.screen').append(document.createTextNode(num));
+
+
+    });
+
+	$('#7').on('click', function() {
+     var num = 7;
+        $('.screen').append(document.createTextNode(num));
+
+
+    });
+
+	$('#8').on('click', function() {
+     var num = 8;
+        $('.screen').append(document.createTextNode(num));
+
+
+    });
+
+	$('#9').on('click', function() {
+     var num = 9;
+        $('.screen').append(document.createTextNode(num));
+
+
+    });
+
+
+	$('#0').on('click', function() {
+     var num = 0;
+        $('.screen').append(document.createTextNode(num));
+
+
+    });
 
 
 
