d4ce9a74d5ac9a599809982c1a9f22af9c2f30a9
Tyler <tahbristol@gmail.com>
2016-12-07 23:37:18 -0500

    first commit of calculator

---
+++ calc.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    //starting with a clear screen and binding the clear button.
+    $('input').val('');
+
+    $('#clear').on('click', function() {
+        $('input').val('');
+    });
+
+
+    $('#1').on('click', function() {
+        $('input').val(1);
+
+    })
+
+
+
+
+
+
+});
